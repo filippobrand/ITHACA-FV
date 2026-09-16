ac7d8c844bb4d82f394e26db2bbd7da90957cb13
Valerio <valerio.di.domenico@vki.ac.be>
2026-09-16 16:13:03 +0200

    Added a base class for LSPG ROMs

---
+++ src/ITHACA_ROMPROBLEMS/ReducedLSPG/ReducedLSPG.C
@@ -0,0 +1,20 @@
+#include "ReducedLSPG.H"
+
+ReducedLSPG::ReducedLSPG(int argc, char* argv[])
+{
+  _args = autoPtr<argList>(
+                new argList(argc, argv));
+
+  if (!_args->checkRootCase())
+  {
+      Foam::FatalError.exit();
+  }
+  argList& args = _args();
+  #include "createTime.H"
+  #include "createMesh.H"
+  _pimple = autoPtr<pimpleControl>(
+                new pimpleControl(
+                    mesh));
+  #include "createFvOptions.H"
+}
+
